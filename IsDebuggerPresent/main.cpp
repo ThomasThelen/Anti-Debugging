abdca045d97c32433d41e314f1a8563ab18210b0
Thomas Thelen <tommythelen@gmail.com>
2016-01-22 13:36:46 -0800

    Cleaned Code

--- IsDebuggerPresent/main.cpp
+++ IsDebuggerPresent/main.cpp
@@ -2,14 +2,17 @@
 #include <windows.h>
 #include <winbase.h>
 #include <String>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+/*
+Uses the return value of IsDebuggerPresent to make the check. 
+*/
+
+int main(int argc, char** argv)
+{
 	
-	std::string aaa="Chance";
 	if (IsDebuggerPresent() == TRUE)
 	{
-		MessageBoxA(NULL, aaa.c_str(), "Error: Debugger", MB_OK);
+		MessageBoxA(NULL, "There is currently a debugger attached to the process.", "Error: Debugger", MB_OK);
 		return 0;
 	}
 	else
